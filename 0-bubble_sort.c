f889e85ab78874f9bf9b620d1ab10bba3f7ca2e0
zyadhany <zeyad.hany2003@gmail.com>
2023-11-21 01:42:04 +0200

    added task

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort array.
+ * @array: given array.
+ * @size: size of array
+ *
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
+
+}
---
+++ sort.h
@@ -0,0 +1,30 @@
+#ifndef _SORT_H
+#define _SORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+
+/*print functions*/
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/*sort functions*/
+void bubble_sort(int *array, size_t size);
+
+#endif
